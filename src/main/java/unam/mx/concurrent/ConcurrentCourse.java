11ee257cae6f3733e508afd2365eaf1a4e83520b
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-08-22 19:37:13 -0600

    Repository initialization.

---
+++ src/main/java/unam/mx/concurrent/ConcurrentCourse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package unam.mx.concurrent;
+
+/**
+ *
+ * @author miguel
+ */
+public class ConcurrentCourse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
