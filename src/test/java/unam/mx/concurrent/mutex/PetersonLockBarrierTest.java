8ca186bee04aa5bbff17ee1925d65475d4898f21
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-08-26 14:00:00 -0600

    Added a new version of the peterson's lock test using barriers.

---
+++ src/test/java/unam/mx/concurrent/mutex/PetersonLockBarrierTest.java
@@ -0,0 +1,63 @@
+package unam.mx.concurrent.mutex;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
+
+/**
+ *
+ * @author miguel
+ */
+public class PetersonLockBarrierTest {
+
+    private final static int THREADS = 2;
+    private final static int COUNT = 1024 * 1000;
+    private final static int PER_THREAD = COUNT / THREADS;
+    private final static int TIMEOUT = 10000;
+    private final Thread[] threads = new Thread[THREADS];
+    private int counter = 0;
+
+    private final PetersonLock instance = new PetersonLock();
+
+    @RepeatedTest(100)
+    public void testPetersonLock() throws InterruptedException {
+        System.out.println("Executing test for the Peterson's Lock");
+        System.out.println("Available processors: " + THREADS);
+        long time = System.currentTimeMillis();
+        Runnable barrierAction = () -> {
+            System.out.println("All threads already joined");
+        };
+        CyclicBarrier barrier = new CyclicBarrier(THREADS, barrierAction);
+        ThreadID.reset();
+        for (int i = 0; i < THREADS; i++) {
+            threads[i] = new Thread(() -> {
+                try {
+                    barrier.await();
+                    for (int t = 0; t < PER_THREAD; t++) {
+                        instance.lock();
+                        try {
+                            counter = counter + 1;
+                        } finally {
+                            instance.unlock();
+                        }
+                    }
+                } catch (InterruptedException | BrokenBarrierException ex) {
+                    Logger.getLogger(PetersonLockBarrierTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+        }
+        for (int i = 0; i < THREADS; i++) {
+            threads[i].start();
+        }
+        for (int i = 0; i < THREADS; i++) {
+            threads[i].join(TIMEOUT);
+        }
+        Assertions.assertEquals(COUNT, counter);
+        time = System.currentTimeMillis() - time;
+        System.out.println(String.format("The value for the counter is: %d", counter));
+        System.out.println(String.format("Total time: %d ms", time));
+    }
+}
